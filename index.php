b128996bea9c79dcd62e3d15404a6c09b2c83810
Salimova Lala <allalalsalimova@gmail.com>
2021-10-22 17:59:28 +0200

    homework-2

--- index.php
+++ index.php
@@ -5,14 +5,17 @@ require "functions.php";
 
 $inputAge = readline($prompt = "Enter your age: ");
 $count = 1;
-if (getError($inputAge))
+if (!getError($inputAge))
 {
-	foreach ($movies as $movie)
+	echo "error";
+	return false;
+}
+foreach ($movies as $movie)
+{
+	if ($movie["age_restriction"] > $inputAge)
 	{
-		if ($movie["age_restriction"] <= $inputAge)
-		{
-			print($count . ". " . formattedMovie($movie) . "\n");
-			++$count;
-		}
+		continue;
 	}
+	print($count . ". " . formattedMovie($movie) . "\n");
+	++$count;
 }
